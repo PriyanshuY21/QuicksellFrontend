66b8cfecd4570b4a929990b82850d24c23002671
Priyanshu <106882732+PriyanshuY21@users.noreply.github.com>
2024-11-12 16:24:11 +0530

    hahs

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./Card.css";
-import { MoreHorizontal } from 'react-feather';
 
 import Backlog from '../Images/Backlog.svg';
 import Todo from '../Images/To-do.svg';
@@ -8,81 +7,94 @@ import Done from '../Images/Done.svg';
 import InProgress from '../Images/in-progress.svg';
 import Cancelled from '../Images/Cancelled.svg';
 
+import Urgent from '../Images/SVG - Urgent Priority colour.svg';
+import Nope from '../Images/No-priority.svg';
+import Low from '../Images/Img - Low Priority.svg';
+import Medium from '../Images/Img - Medium Priority.svg';
+import High from '../Images/Img - High Priority.svg';
+
 const Card = ( props ) => {
     
     const logoReference = {
-      "Backlog": <img src={Backlog} className='logo-pic' alt="Backlog" />,
-      "Todo": <img src={Todo} className='logo-pic' alt="Todo" />,
-      "In progress": <img src={InProgress} className='logo-pic' alt="In progress" />,
-      "Done": <img src={Done} className='logo-pic' alt="Done" />,
-      "Cancelled": <img src={Cancelled} className='logo-pic' alt="Cancelled" />
-  };
+        "Backlog": <img src={Backlog} className='logo-pic' alt="Backlog" />,
+        "Todo": <img src={Todo} className='logo-pic' alt="Todo" />,
+        "In progress": <img src={InProgress} className='logo-pic' alt="In progress" />,
+        "Done": <img src={Done} className='logo-pic' alt="Done" />,
+        "Cancelled": <img src={Cancelled} className='logo-pic' alt="Cancelled" />
+    };
 
-  const { ticket, users } = props;
+    const priorityIcon = {
+        0: <img src={Nope} className='priority-icon' alt="No Priority" />,
+        1: <img src={Urgent} className='priority-icon' alt="Urgent" />,
+        2: <img src={Low} className='priority-icon' alt="Low" />,
+        3: <img src={Medium} className='priority-icon' alt="Medium" />,
+        4: <img src={High} className='priority-icon' alt="High" />,
+    };
 
-  const getUserById = (userId) => {
-    return users.find((user) => user.id === userId);
-  };
+    const { ticket, users } = props;
 
-  const getInitials = (name) => {
-    const nameParts = name.split(' ');
-    const firstName = nameParts[0];
-    const lastName = nameParts.length > 1 ? nameParts[1] : '';
-    const initials = `${firstName.charAt(0)}${lastName ? lastName.charAt(0) : ''}`;
-    return initials.toUpperCase();
-  };
+    const getUserById = (userId) => {
+        return users.find((user) => user.id === userId);
+    };
 
-  const getRandomColor = () => {
-    const letters='0123456789ABCDEF';
-    let color='#';
-    for (let i=0;i<6;i++) {
-      color+=letters[Math.floor(Math.random()*7)];
-    }
-    return color;
-  };
+    const getInitials = (name) => {
+        const nameParts = name.split(' ');
+        const firstName = nameParts[0];
+        const lastName = nameParts.length > 1 ? nameParts[1] : '';
+        const initials = `${firstName.charAt(0)}${lastName ? lastName.charAt(0) : ''}`;
+        return initials.toUpperCase();
+    };
 
-  const user = getUserById(ticket.userId);
-  const initials = user ? getInitials(user.name) : '';
+    const getRandomColor = () => {
+        const letters='0123456789ABCDEF';
+        let color='#';
+        for (let i=0;i<6;i++) {
+            color+=letters[Math.floor(Math.random()*7)];
+        }
+        return color;
+    };
 
-  return (
-    <div className='card'>
-      <div className='card-top'>
-        <p className='card-title'>{props.ticket.id}</p>
-        {props.groupBy !== 'user' && (
-            <div
-            className="user-pic"
-            style={{
-              backgroundColor: getRandomColor()
-            }}
-          >
-            {initials}
-          </div>
-        )}
+    const user = getUserById(ticket.userId);
+    const initials = user ? getInitials(user.name) : '';
 
-      </div>
-      <div className='card-middle'>
-        {props.groupBy !== 'status' && logoReference[props.ticket.status]}
-        <p className='card-desc bold'>
-          {props.ticket.title}
-        </p>
-      </div>
-      <div className='card-bottom'>
-        {props.groupBy !== 'priority' && (
-          <div className='component-1 pointer'>
-            <MoreHorizontal className='icon' />
-          </div>
-        )}
-        <div className='component-2 pointer'>
-          <img
-            src="https://cdn-icons-png.flaticon.com/128/5720/5720434.png"
-            className='icon'
-            alt='logo'
-          />
-          <p>{props.ticket.tag[0]}</p>
+    return (
+        <div className='card'>
+            <div className='card-top'>
+                <p className='card-title'>{ticket.id}</p>
+                {props.groupBy !== 'user' && (
+                    <div
+                        className="user-pic"
+                        style={{
+                            backgroundColor: getRandomColor()
+                        }}
+                    >
+                        {initials}
+                    </div>
+                )}
+            </div>
+            <div className='card-middle'>
+                {props.groupBy !== 'status' && logoReference[ticket.status]}
+                <p className='card-desc bold'>
+                    {ticket.title}
+                </p>
+            </div>
+            <div className='card-bottom'>
+                {props.groupBy !== 'priority' && (
+                    <div className='priority-box'>
+                        {priorityIcon[ticket.priority]}
+                    </div>
+                )}
+                <div className='feature-box'>
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/128/5720/5720434.png"
+                        className='feature-icon'
+                        alt='Feature Request'
+                    />
+                    <p>{ticket.tag[0]}</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-export default Card
+export default Card;

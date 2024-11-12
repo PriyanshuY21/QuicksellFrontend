41ae0f3116023aba590e1dfe1bf678c384a94031
Priyanshu <106882732+PriyanshuY21@users.noreply.github.com>
2024-11-12 14:24:36 +0530

    Updates

---
+++ precache-manifest.bc4e356d55fe1490a767c8a9f6eac249.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a403d6fe4fa9e19f7e8593c98a969db",
+    "url": "/QuicksellFrontend/index.html"
+  },
+  {
+    "revision": "8c5faeebe440164cbdb9",
+    "url": "/QuicksellFrontend/static/css/main.12281049.chunk.css"
+  },
+  {
+    "revision": "da78b290eef074c7d7db",
+    "url": "/QuicksellFrontend/static/js/2.4c8d5b32.chunk.js"
+  },
+  {
+    "revision": "29fe86816a802490cec5",
+    "url": "/QuicksellFrontend/static/js/3.d3e4d6ea.chunk.js"
+  },
+  {
+    "revision": "8c5faeebe440164cbdb9",
+    "url": "/QuicksellFrontend/static/js/main.8b45c13a.chunk.js"
+  },
+  {
+    "revision": "2d83d376f250d4e57f50",
+    "url": "/QuicksellFrontend/static/js/runtime~main.00465972.js"
+  },
+  {
+    "revision": "47973b095119ca88f2cf7435ae64b951",
+    "url": "/QuicksellFrontend/static/media/Backlog.47973b09.svg"
+  },
+  {
+    "revision": "15c84b141088b15567be2fc776da7576",
+    "url": "/QuicksellFrontend/static/media/Cancelled.15c84b14.svg"
+  },
+  {
+    "revision": "b96d92a33a5b1ec307931b7e2a8d7d32",
+    "url": "/QuicksellFrontend/static/media/Display.b96d92a3.svg"
+  },
+  {
+    "revision": "ee99e204ff8124cf3efad786a4035a57",
+    "url": "/QuicksellFrontend/static/media/Done.ee99e204.svg"
+  },
+  {
+    "revision": "991aa8ffc2f2062e7a647ea42988b361",
+    "url": "/QuicksellFrontend/static/media/To-do.991aa8ff.svg"
+  },
+  {
+    "revision": "6f7f6c8e224d8356b240a937ce34069f",
+    "url": "/QuicksellFrontend/static/media/in-progress.6f7f6c8e.svg"
+  }
+]);
